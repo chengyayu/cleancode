c08ba4a3d0e26178c698b36b310f1e91d5dc3be0
Yayu <tim.yayu.cheng@gmail.com>
2023-10-27 02:03:41 +0000

    Refactor: add empty line to source data for testing filter func

--- replace-loop-with-piepeline/main.go
+++ replace-loop-with-piepeline/main.go
@@ -6,6 +6,7 @@ import (
 
 const offices = `office, country, telephone
 Chicago, USA, +1 312 373 1000
+
 Beijing, China, +86 4008 900 505
 Bangalore, India, +91 80 4064 9570
 Porto Alegre, Brazil, +55 51 3079 3550
