bd921c648b329f958d18c9ce0c17f405fc8fb422
yayu <tim.yayu.cheng@gmail.com>
2023-10-28 12:16:45 +0800

    Refactor: split loop

--- split-loop/f2.go
+++ split-loop/f2.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func getYoungestAgeAndTotalSalaryV2(peoples []*People) string {
 
-	return ""
+	youngestAge := math.MaxInt
+	for _, p := range peoples {
+		if p.Age < youngestAge {
+			youngestAge = p.Age
+		}
+	}
+
+	totalSalary := 0
+	for _, p := range peoples {
+		totalSalary += p.Salary
+	}
+
+	return fmt.Sprintf("youngestAge: %d, totalSalary: %d", youngestAge, totalSalary)
 }
