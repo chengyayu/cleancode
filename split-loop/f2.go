e53c5f8567079f49197dc2ca3dd40ea0a5b998dc
yayu <tim.yayu.cheng@gmail.com>
2023-10-28 12:22:12 +0800

    Refactor: extract method

--- split-loop/f2.go
+++ split-loop/f2.go
@@ -7,17 +7,27 @@ import (
 
 func getYoungestAgeAndTotalSalaryV2(peoples []*People) string {
 
+	youngestAge := getYoungestAge(peoples)
+
+	totalSalary := getTotalSalary(peoples)
+
+	return fmt.Sprintf("youngestAge: %d, totalSalary: %d", youngestAge, totalSalary)
+}
+
+func getTotalSalary(peoples []*People) int {
+	totalSalary := 0
+	for _, p := range peoples {
+		totalSalary += p.Salary
+	}
+	return totalSalary
+}
+
+func getYoungestAge(peoples []*People) int {
 	youngestAge := math.MaxInt
 	for _, p := range peoples {
 		if p.Age < youngestAge {
 			youngestAge = p.Age
 		}
 	}
-
-	totalSalary := 0
-	for _, p := range peoples {
-		totalSalary += p.Salary
-	}
-
-	return fmt.Sprintf("youngestAge: %d, totalSalary: %d", youngestAge, totalSalary)
+	return youngestAge
 }
